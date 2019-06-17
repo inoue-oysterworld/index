4b7f6e0e930456cb52ab52003082672bb1bea5f5
inoue-oysterworld <51821151+inoue-oysterworld@users.noreply.github.com>
2019-06-17 10:53:29 +0900

    packaging mysite

---
+++ oysterworld/src/main/java/jp/oysterworld/OysterworldApplication.java
@@ -0,0 +1,13 @@
+package jp.oysterworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OysterworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OysterworldApplication.class, args);
+	}
+
+}
